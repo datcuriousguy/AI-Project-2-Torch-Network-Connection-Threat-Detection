dda4a5ca5ba07239991728238a3bb6810f702bde
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2025-05-06 18:17:09 +0530

    Create model.py
    Added code to access the database, retrieve the training data (fabricated), and clean off all null values in the data. columns are
    Protocol_type, Service, Flag, Src_bytes, Dst_bytes, Count, Serror_rate, Rerror_rate, Malicious

---
+++ model.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
+import pymysql
+import mysql.connector
+import numpy as np
+
+""" Retrieving the data from the mysql database. For some reason, using pymysql resolved the sha_password_authentication
+    error that using mysql.connector would yield. """
+
+def load_data():
+    conn = pymysql.connect(
+        host='localhost',
+        user='root',
+        password='password',
+        database='nn_project_2_database'
+    )
+
+    """
+    predefined query to fetch all the columns of training data - we need to use oit to train the mo del
+    """
+
+    query = "SELECT Protocol_type, Service, Flag, Src_bytes, Dst_bytes, Count, Serror_rate, Rerror_rate, Malicious FROM connection_data"
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df.dropna()  # we can use dropna() function to drop rows with null values to clean the data.
